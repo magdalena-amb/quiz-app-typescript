3b30334ec2c91ba0ddef09b3a3fcfa58e63fa624
Magdalena Ambrozy <magda.ambrozy@gmail.com>
2022-04-06 15:45:19 +0200

    Add API.ts

--- src/components/QuestionCard.tsx
+++ src/components/QuestionCard.tsx
@@ -1,9 +1,37 @@
-import React from 'react'
+import React from "react";
 
 interface QuestionCardProps {
-
+  question: string;
+  answers: string[];
+  callback: any;
+  userAnswer: any;
+  questionNr: number;
+  totalQuestions: number;
 }
 
-export const QuestionCard: React.FC<QuestionCardProps> = ({}) => {
-        return ();
-}
+export const QuestionCard: React.FC<QuestionCardProps> = ({
+  question,
+  answers,
+  callback,
+  userAnswer,
+  questionNr,
+  totalQuestions,
+}) => {
+  return (
+    <div>
+      <p className="number">
+        Question: {questionNr} / {totalQuestions}
+      </p>
+      <p dangerouslySetInnerHTML={{ __html: question }} />
+      <div>
+        {answers.map((answer) => (
+          <div>
+            <button disabled={userAnswer} onClick={callback}>
+              <span dangerouslySetInnerHTML={{ __html: answer }} />
+            </button>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+};
